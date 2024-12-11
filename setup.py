0a87ea3746d7657b1c02aa97f48485dab50b76a7
Umme-Ayman-Khanam <aymankhan1307@gmail.com>
2024-12-11 21:15:47 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='word-detector',
+    name='OCR',
     version='1.0.0',
-    description='Word Detector',
-    author='Harald Scheidl',
+    description='Optical Character Recognition',
+    author='Umme Ayman Khanam',
     packages=['word_detector'],
-    url="https://github.com/githubharald/WordDetector",
+    url="https://github.com/Umme-Ayman-Khanam/Optical-Character-Recognition",
     install_requires=['numpy', 'sklearn', 'opencv-python'],
     python_requires='>=3.7'
 )
